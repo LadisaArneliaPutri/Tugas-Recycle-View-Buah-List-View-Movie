35abe63d73c55d4ba1b874529b3a641d515e2a07
LadisaArneliaPutri <ldisa52@gmail.com>
2024-10-14 10:30:06 +0700

    Tugas Ladisa arnelia Putri

---
+++ app/src/main/java/com/ladisa/project_kotlin_mobileapp/model/ModelBuku.kt
@@ -0,0 +1,6 @@
+package com.ladisa.project_kotlin_mobileapp.model
+
+data class ModelBuku(
+    val judul: String,
+    val penulis: String
+)
---
+++ app/src/main/java/com/ladisa/project_kotlin_mobileapp/model/ModelMovie.kt
@@ -0,0 +1,10 @@
+package com.ladisa.project_kotlin_mobileapp.model
+
+data class ModelMovie(
+    var title: String,
+    var image: Int
+) {
+    class MyViewHolder {
+
+    }
+}
